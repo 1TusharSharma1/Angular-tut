2f41a62090aa26f8ff464ee1b612a773d0975d7b
1TusharSharma1 <tush.sharma2504@gmail.com>
2025-01-10 15:14:22 +0530

    Concept: Filters

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 let myModule = angular.module('moduleName', []);
 
 
-myModule.controller('myController', [$scope, function($scope) { //helps protect variable names from minification
-    $scope.message = 'Hello, Angular!';
-    $scope.anotherMessage = 'Message2';
+myModule.controller('myController', ['$scope', function($scope) {
+    $scope.obj = [{
+        name: 'Alice Doe',
+        age: 25,
+
+    },
+    {
+        name: 'Jane Doe',
+        age: 26,
+
+    },
+    {
+        name: 'Zayn Doe',
+        age: 27,
+    },
+    {
+        name: 'Shaun Doe',
+        age: 28,
+    }
+
+];
 
 }]);
