52f2a4b7030dba7b1d970e0292dda3be22b394ab
1TusharSharma1 <tush.sharma2504@gmail.com>
2025-01-10 12:58:00 +0530

    Enhanced controller function's dependency definement

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let myModule = angular.module('moduleName', []);
 
 
-myModule.controller('myController', function($scope) {
+myModule.controller('myController', [$scope, function($scope) { //helps protect variable names from minification
     $scope.message = 'Hello, Angular!';
     $scope.anotherMessage = 'Message2';
-});
+
+}]);
