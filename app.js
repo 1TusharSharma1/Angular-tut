ac4dece2ac051d3f388c9cf8b51b838b4f80d118
1TusharSharma1 <tush.sharma2504@gmail.com>
2025-01-13 16:27:31 +0530

    Concept: Custom Directives

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
 let myModule = angular.module("moduleName", []);
+myModule.directive("customDirective", function () {
 
+  function customFunction($scope, element, attrs) {
+    $scope.text = "Hello World";
+    $scope.changeTxt = function(newTxt) {
+      $scope.text = newTxt;
+
+    } 
+    }
+    return {
+      restrict: "E",
+      scope : {},
+      template: "<h1 ng-click='changeTxt(\"Custom Directives working fine!!!\")'>{{text}}</h1>",
+      link: customFunction
+  }
+});
 myModule.controller("myController", [
   "$scope", function ($scope) {
-    $scope.obj= [{
-      name: "John",
-      age: 25,
-      address: "New York"
-    },
-    {
-      name: "Peter",
-      age: 30,
-      address: "California"
-    },
-    {
-      name: "Sam",
-      age: 35,
-      address: "Texas"
-    },
-    {
-      name: "Ryan",
-      age: 20,
-      address: "Florida"
-    }
-  ,{
-    name: "Mike",
-    age: 18,
-    address: "Washington"
-  }];
+    $scope.obj={}
 }
 ]);
